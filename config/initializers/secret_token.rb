4ae6ced0bc84a56bef81e9e83574b9415ecbfb28
Tyler Schuett <zca.tyler@gmail.com>
2013-07-22 13:00:40 -0700

    Created QC Proto App and added posts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qcproto::Application.config.secret_token = 'cfa3203d1abafc7ea0f6943c6f99ac19b595bc4aef460adf0671ecba5c26a23ac92facb345e436ab3328c8d65809dc165f4c946327813cb9e589a1e0f818fdd6'
